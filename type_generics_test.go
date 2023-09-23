6f122b409b1eac4b2ff462a56dc423a7b015c0f8
dyaksa <diasnour0395@gmail.com>
2023-09-23 20:48:57 +0700

    type generics

---
+++ type_generics_test.go
@@ -0,0 +1,21 @@
+package fund_go_generics
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Print[T any](param T) T {
+	fmt.Println(param)
+	return param
+}
+
+func Test_Type(t *testing.T) {
+	var name string = Print[string]("Putri")
+	assert.Equal(t, "Putri", name)
+
+	var umur int = Print[int](21)
+	assert.Equal(t, 21, umur)
+
+}
