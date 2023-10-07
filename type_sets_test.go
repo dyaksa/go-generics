8bd3d8d4ae357f3fd9699fddd317c0ca26dc9b62
dyaksa <diasnour0395@gmail.com>
2023-10-07 17:02:06 +0700

    type aproximation

--- type_sets_test.go
+++ type_sets_test.go
@@ -5,8 +5,10 @@ import (
 	"testing"
 )
 
+type Age int
+
 type Number interface {
-	int | int8 | int16 | int32 | int64
+	~int | int8 | int16 | int32 | int64
 }
 
 func Min[T Number](param1, param2 T) T {
@@ -19,5 +21,7 @@ func Min[T Number](param1, param2 T) T {
 
 func TestNumber(t *testing.T) {
 	minimal := Min[int](1, 2)
+	minn := Min[Age](1, 2)
 	assert.Equal(t, 1, minimal)
+	assert.Equal(t, Age(1), minn)
 }
